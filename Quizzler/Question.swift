d67f38445aa8d7e8b8cfeac1bdd632301af2d15d
chaospredictor <kuzi81@gmail.com>
2017-09-10 09:20:41 +0300

    Question class added

---
+++ Quizzler/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Master on 10/09/2017.
+//  Copyright © 2017 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    let questionText : String
+    let answer : Bool
+}
