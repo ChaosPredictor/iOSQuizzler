c11655ea3c53bffddb9c5e86598a50a25818320b
chaospredictor <kuzi81@gmail.com>
2017-09-12 12:23:42 +0300

    QuestionBank class added

--- Quizzler/Question.swift
+++ Quizzler/Question.swift
@@ -11,4 +11,9 @@ import Foundation
 class Question {
     let questionText : String
     let answer : Bool
+
+    init(text: String, correctAnswer: Bool ) {
+        questionText = text
+        answer = correctAnswer
+    }
 }
---
+++ Quizzler/QuestionBank.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionBank.swift
+//  Quizzler
+//
+//  Created by Master on 12/09/2017.
+//  Copyright © 2017 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class QuestionBank {
+    
+}
